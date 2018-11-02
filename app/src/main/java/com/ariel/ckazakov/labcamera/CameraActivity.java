c70a00e36703a482563633503f220b143dd85036
CKazakov <Constantine.Kazakov@harman.com>
2018-11-02 23:49:52 +0200

    Added Main activity basic design

--- app/src/main/java/com/ariel/ckazakov/labcamera/CameraActivity.java
+++ app/src/main/java/com/ariel/ckazakov/labcamera/CameraActivity.java
@@ -1,13 +1,50 @@
 package com.ariel.ckazakov.labcamera;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
 
 public class CameraActivity extends AppCompatActivity {
+
+    ImageView photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        Button buttonCapture = findViewById(R.id.buttonCapture);
+        photo = findViewById(R.id.photo);
+        TextView textView = findViewById(R.id.textView);
+
+        buttonCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) (data != null ? requireNonNull(data.getExtras()).get("data") : null);
+        photo.setImageBitmap(bitmap);
     }
 }
--- app/src/main/java/com/ariel/ckazakov/labcamera/MainActivity.java
+++ app/src/main/java/com/ariel/ckazakov/labcamera/MainActivity.java
@@ -1,13 +1,26 @@
 package com.ariel.ckazakov.labcamera;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button2);
+        EditText editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CameraActivity.class));
+            }
+        });
     }
 }
